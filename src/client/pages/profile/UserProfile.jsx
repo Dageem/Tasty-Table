4404e5320561a24dc6a092ce561fe5338d3c4db7
DanielJerman <danieljerman97@gmail.com>
2023-10-27 04:24:49 -0400

    styling

--- src/client/pages/profile/UserProfile.jsx
+++ src/client/pages/profile/UserProfile.jsx
@@ -25,7 +25,7 @@ function UserProfile() {
     data: recipesData,
     isLoading: isRecipesLoading,
     isError: isRecipesError,
-    refetch  // <-- Add this to get the refetch function
+    refetch  
 } = useGetRecipesByUserIdQuery(userId, { skip: !userId });
 
   
@@ -54,7 +54,7 @@ function UserProfile() {
   const onDelete = async (recipeId) => {
     try {
       await deleteRecipe(recipeId).unwrap();
-      refetch();  // <-- Call refetch here to reload the recipes
+      refetch();  
     } catch (error) {
       console.log("Error during deletion:", error);
     }
@@ -83,84 +83,86 @@ function UserProfile() {
   
   return (
     <div className="big-container">
-      {/* User Profile Header */}
-      <div className="flex flex-col mb-6 mt-5 ml-5">
-        <h1 className="text-xl font-extrabold">{user.username}'s Profile</h1>
-        <h2>Number of recipes: {recipes?.length || 0}</h2>
-        <h2>Number of posts: {postsData.length}</h2>
-      </div>
+  {/* User Profile Header */}
+  <div className="flex flex-col mb-6 mt-5 ml-5">
+    <h1 className="text-xl font-extrabold">{user.username}'s Profile</h1>
+    <h2>Number of recipes: {recipes?.length || 0}</h2>
+    <h2>Number of posts: {postsData.length}</h2>
+  </div>
 
-      <Tabs className="" value="recipesAndPosts">
-        <TabsHeader>
-          <Tab value="recipes">Recipes</Tab>
-          <Tab value="posts">Posts</Tab>
-        </TabsHeader>
-        <TabsBody>
-        <TabPanel value="recipes">
-          <div className="recipes-container flex flex-wrap justify-center mt-10 w-300">
-              {recipes.map((recipe) => (
-                  <div className="m-4 relative group w-1/4" key={recipe.id}>
-                 {/* Image */}
-                 <div className="w-full h-96 relative overflow-hidden">
-                    <img 
-                      src={recipe.imageUrl} 
-                      alt="Recipe" 
-                      className="w-full max-h-full object-contain absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                    />
-                    <div className="absolute inset-0 bg-black bg-opacity-60 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                      <div className="text-white p-4">
-                        <h2 className="text-lg font-bold mb-2">{recipe.name}</h2>
-                        <p>
-                          { 
-                            recipe.details.split(" ").slice(0, 70).join(" ")
-
-                            + (recipe.details.split(" ").length > 70 ? "..." : "")
-                          }
-                        </p>
-                        </div>
-                    </div>
-                  </div>
-
-                {/* Edit and Delete buttons */}
-                <div className="mt-4 flex justify-between">
-                    <Link to={`/editrecipe/${recipe.id}`} className="flex-1 mr-2">
-                        <button className="bg-blue-900 text-white p-2 rounded w-full">
-                            Edit Recipe
-                        </button>
-                    </Link>
-                    <button 
-                        className="bg-red-500 text-white p-2 rounded flex-1" 
-                        onClick={(e) => {
-                            onDelete(recipe.id);
-                        }}>
-                        Delete Recipe
-                    </button>
-                            </div>
-                        </div>
-                    ))}
-
-                    <div className="mt-10 w-full flex justify-center">
-                        <Link to="/recipesubmit">
-                            <button className="bg-green-300 text-white p-4 rounded w-full">
-                            Add a New Recipe !
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-        </TabPanel>
-          <TabPanel value="posts">
-            <div className="">
-              {postsData.map((post) => (
-                <div className="apple" key={post.id}>
-                  <p>Title: {post.title}</p>
-                  <p>Content: {post.content}</p>
-                </div>
-              ))}
+  <Tabs className="" value="recipesAndPosts">
+    <TabsHeader>
+      <Tab value="recipes">Recipes</Tab>
+    </TabsHeader>
+    <TabsBody>
+      <TabPanel value="recipes">
+      <div className="recipes-container flex flex-wrap justify-end mt-10">
+    {recipes.map((recipe) => (
+        <div className="group m-4 w-3/4 p-4 border border-gray-300 bg-gradient-to-r from-purple-100 via-purple-50 to-gray-100 rounded flex relative" key={recipe.id}>
+            {/* Image */}
+            <div className="mr-4 flex-none">
+                <img 
+                    src={recipe.imageUrl} 
+                    alt="Recipe" 
+                    className="w-24 h-24 object-contain"
+                />
             </div>
-          </TabPanel>
-        </TabsBody>
-      </Tabs>
-    </div>
+            {/* Title */}
+            <h2 className="flex-grow text-lg font-bold text-center mb-2">{recipe.name}</h2>
+            <div className="absolute inset-0 bg-gray-500 opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
+
+            {/* Details Overlay */}
+            <div className="absolute inset-0 flex flex-col justify-center p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <p className="text-sm text-center text-white" style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.7)' }}>
+                    { 
+                        recipe.details.split(" ").slice(0, 20).join(" ")
+                        + (recipe.details.split(" ").length > 20 ? "..." : "")
+                    }
+                </p>
+            </div>
+
+            {/* Edit and Delete buttons*/}
+            <div className="absolute bottom-0 right-0 flex flex-col space-y-2 p-4">
+                <Link to={`/editrecipe/${recipe.id}`}>
+                    <button className="bg-blue-900 text-white p-2 rounded">
+                        Edit Recipe
+                    </button>
+                </Link>
+                <button 
+                    className="bg-red-500 text-white p-2 rounded" 
+                    onClick={() => onDelete(recipe.id)}
+                >
+                    Delete Recipe
+                </button>
+            </div>
+        </div>
+    ))}          
+          <div className="mt-10 w-full flex justify-center">
+            <Link to="/recipesubmit">
+              <button className="bg-green-300 text-white p-4 rounded w-full max-w-md">
+                Add a New Recipe !
+              </button>
+            </Link>
+          </div>
+
+        </div>
+      </TabPanel>
+
+      <TabPanel value="posts">
+        <div className="">
+          {postsData.map((post) => (
+            <div className="apple" key={post.id}>
+              <p>Title: {post.title}</p>
+              <p>Content: {post.content}</p>
+            </div>
+          ))}
+        </div>
+      </TabPanel>
+
+    </TabsBody>
+  </Tabs>
+</div>
+
   );
 }
 
