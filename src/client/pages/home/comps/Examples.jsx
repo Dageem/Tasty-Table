be35342585a9a884de1304d59cc1df62b9760149
jacobhogsten <hogsten1996@gmail.com>
2023-10-27 11:28:36 -0400

    tweaked images and queries

--- src/client/pages/home/comps/Examples.jsx
+++ src/client/pages/home/comps/Examples.jsx
@@ -1,50 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Examples() {
   return (
-      <section className="">
-        <h1 className="text-3xl font-bold mt-6 mb-2">Popular Recipes</h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 pb-4">
-          <Link to="/categories/easy">
-            <div className="relative hover:cursor-pointer hover:opacity-70">
-              <img
-                src="https://www.acouplecooks.com/wp-content/uploads/2021/03/Cheese-Tortellini-011.jpg"
-                alt=""
-                className="w-full h-[350px]"
-              />
-              <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-2 bg-white bg-opacity-80 rounded text-l font-bold">
-                Easy Recipes
-              </div>
+    <section className="">
+      <h1 className="text-3xl font-bold mt-6 mb-2">Popular Recipes</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 pb-4">
+        <Link to="/categories/easy">
+          <div className="relative hover:cursor-pointer hover:opacity-70">
+            <div
+              className="w-full h-[350px] bg-cover bg-center"
+              style={{
+                backgroundImage: `url(https://www.acouplecooks.com/wp-content/uploads/2021/03/Cheese-Tortellini-011.jpg)`,
+              }}
+            ></div>
+            <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-2 bg-white bg-opacity-80 rounded text-l font-bold">
+              Easy Recipes
             </div>
-          </Link>
+          </div>
+        </Link>
+        
+        {/* Conditionally render the "Quick Recipes" section for XL screens or bigger */}
+        <div className="hidden xl:block">
           <Link to="/categories/quick">
             <div className="relative hover:cursor-pointer hover:opacity-70">
-              <img
-                src="https://cdn.britannica.com/13/234013-050-73781543/rice-and-chorizo-burrito.jpg"
-                alt=""
-                className="w-full h-[350px]"
-              />
+              <div
+                className="w-full h-[350px] bg-cover bg-center"
+                style={{
+                  backgroundImage: `url(https://cdn.britannica.com/13/234013-050-73781543/rice-and-chorizo-burrito.jpg)`,
+                }}
+              ></div>
               <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-2 bg-white bg-opacity-80 rounded text-l font-bold">
                 Quick Recipes
               </div>
             </div>
           </Link>
-          <Link to="/allrecipes">
-            <div className="relative hover:cursor-pointer hover:opacity-70">
-              <img
-                src="https://sourcehealthcenter.com/wp-content/uploads/2021/09/thanksgiving-table.jpg"
-                alt=""
-                className="w-full h-[350px]"
-              />
-              <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-2 bg-white bg-opacity-80 rounded text-l font-bold">
-                All Recipes
-              </div>
-            </div>
-          </Link>
         </div>
-      </section>
-  )
+
+        <Link to="/allrecipes">
+          <div className="relative hover:cursor-pointer hover:opacity-70">
+            <div
+              className="w-full h-[350px] bg-cover bg-center"
+              style={{
+                backgroundImage: `url(https://sourcehealthcenter.com/wp-content/uploads/2021/09/thanksgiving-table.jpg)`,
+              }}
+            ></div>
+            <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 p-2 bg-white bg-opacity-80 rounded text-l font-bold">
+              All Recipes
+            </div>
+          </div>
+        </Link>
+      </div>
+    </section>
+  );
 }
 
-export default Examples
+export default Examples;
+
