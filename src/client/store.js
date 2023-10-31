9d9c5aa2befd923e85b25456340e6d5c13122c63
dageem <dagtin1@gmail.com>
2023-10-31 14:07:20 -0400

    redux comment

--- src/client/store.js
+++ src/client/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./reducers/api";
 import authReducer from './reducers/auth';
 import searchReducer from "./reducers/api";
+import commentsReducer from "./reducers/commentsSlice"
 
 
 const store = configureStore({
     reducer: {
         [api.reducerPath]: api.reducer,
         auth: authReducer,
-        search: searchReducer
+        search: searchReducer,
+        comments: commentsReducer
    
     },
     middleware: (getDefaultMiddleware) =>
