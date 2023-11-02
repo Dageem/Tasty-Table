e0fd904ffdbfed23c3b5d6e845ec9e8113072de7
dageem <dagtin1@gmail.com>
2023-11-02 10:07:26 -0400

    Map

--- src/client/pages/map/map.jsx
+++ src/client/pages/map/map.jsx
@@ -1,34 +1,78 @@
 import React, { useState } from "react";
 import osmProvider from "./osm-provider";
-import{ MapContainer, TileLayer} from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useRef } from "react";
-// import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
+import "./index.css";
+import { useNavigate } from "react-router-dom";
 
-
-const BasicMap =()=> {
-const [center, setCenter]=useState({lat: 13.084622, lng: 80.248357})
-const Zoom_LEVEL =4;
-const mapRef= useRef();
-
-    return(
-        <>
-        
-        <div className="row">
-            <div className="col text-center">
-                <h2>React-leaflet - Basic Openstreet Maps</h2>
-                <p>Loading basic map using layer from maptiler</p>
-                <div className="col">
-                    <MapContainer center={center} zoom={Zoom_LEVEL} ref={mapRef}>
-                        <TileLayer
-                            url={osmProvider.maptiler.url}
-                            attribution={osmProvider.maptiler.attribution}
-                        />
-                    </MapContainer>
-                </div>
-            </div>
+const BasicMap = () => {
+  const [center, setCenter] = useState({ lat: 13.084622, lng: 1.248357 });
+  const Zoom_LEVEL = 2;
+  const mapRef = useRef();
+  const navigate = useNavigate();
+  const markers = [
+    {
+      geocode: [19.432608, -99.133209],
+      popUp: "mexican",
+    },
+    {
+      geocode: [35.8617, 104.1954],
+      popUp: "chinese",
+    },
+    {
+      geocode: [20.5937, 78.9629],
+      popUp: "indian",
+    },
+    {
+      geocode: [41.8719, 12.5674],
+      popUp: "italian",
+    },
+    {
+      geocode: [39.0742, 21.8243],
+      popUp: "mediterranean",
+    },
+    {
+      geocode: [35.9078, 127.7669],
+      popUp: "Korean",
+    },
+    {
+      geocode: [15.87, 100.9925],
+      popUp: "Thai",
+    },
+  ];
+  const handleMarkerClick = (index) => {
+    navigate(`/categories/${index}`);
+  };
+  return (
+    <>
+      <div className="row">
+        <div className="col text-center  ">
+          <h1>The Tasty Table Map</h1>
+          <p>The Flavors are out of this world!</p>
+          <div className="col">
+            <MapContainer center={center} zoom={Zoom_LEVEL}>
+              <TileLayer
+                url={osmProvider.maptiler.url}
+                attribution={osmProvider.maptiler.attribution}
+              />
+              {markers.map((marker, index) => (
+                <Marker
+                  key={marker.popUp}
+                  position={marker.geocode}
+                  eventHandlers={{
+                    click: () => handleMarkerClick(marker.popUp),
+                  }}
+                >
+                  <Popup>Marker {index}</Popup>
+                </Marker>
+              ))}
+            </MapContainer>
+          </div>
         </div>
+      </div>
     </>
-);
+  );
 };
 
 export default BasicMap;
