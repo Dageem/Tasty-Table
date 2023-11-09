6c9256d82b631e469c0d653ac48939448f10f89d
dageem <dagtin1@gmail.com>
2023-11-09 14:19:14 -0500

    login fix

--- src/client/pages/loginRegister/Login.jsx
+++ src/client/pages/loginRegister/Login.jsx
@@ -38,7 +38,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 min-h-screen">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           className="mx-auto h-21 w-auto"
