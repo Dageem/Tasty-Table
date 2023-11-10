035cb2f0a7cc27684f9be6eb9ddd6174558eb8c3
jacobhogsten <hogsten1996@gmail.com>
2023-11-10 12:11:36 -0500

    took all recipes out of store due to rollover stat

--- src/client/pages/profile/comps/Saved.jsx
+++ src/client/pages/profile/comps/Saved.jsx
@@ -49,13 +49,13 @@ export default function Saved() {
           savedRecipes.map((savedRecipe) => (
             <div
               key={savedRecipe.recipe.id}
-              className="w-full sm:w-1/2 md:w-1/2 xl:w-1/3 2xl:w-1/4 sm:p-3 cursor-pointer"
+              className="w-full sm:w-1/2 md:w-1/2 xl:w-1/3 2xl:w-1/4 sm:p-3 cursor-pointer "
             >
               <Link
                 to={`/recipe/${savedRecipe.recipe.id}`}
-                className="hover:opacity-70"
+                className="hover:opacity-70 "
               >
-                <div className="lg:min-h-[390px]">
+                <div className="lg:min-h-[420px]">
                   <div className="flex items-center justify-center">
                     <img
                       src={savedRecipe.recipe.imageUrl || backupImage}
